914108e34bc93f9ba99877ad7aabdc07607666cb
JGuiraud <jerome31guiraud@gmail.com>
2017-01-04 15:57:06 +0100

    indent

--- js/panda.js
+++ js/panda.js
@@ -1,13 +1,13 @@
 
 $(document).ready(function() {
     $(".panda").click(function() {
-			var type = this.getAttribute("data-type");
-			var tendance = this.getAttribute("data-tendance")
-    		
-    		if (type == null && tendance == null) {
-    			alert("Oups, il n'y a pas de tendance ou de type à afficher")
-    		} else {
-				alert("Le type est : " + type + "." + " " + "La tendance est : " + tendance + ".");   
-    		}
-    });
+     var type = this.getAttribute("data-type");
+     var tendance = this.getAttribute("data-tendance")
+     
+     if (type == null && tendance == null) {
+         alert("Oups, il n'y a pas de tendance ou de type à afficher")
+     } else {
+        alert("Le type est : " + type + "." + " " + "La tendance est : " + tendance + ".");   
+    }
+});
 });
